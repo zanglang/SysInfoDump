a0b078aa0747be64efbda56200c14d119659a144
jerry <jerry@15c49f66-a4e5-453b-9e70-f24e6a7cf82a>
2012-10-03 06:17:20 +0000

    - Removed SysInfo dumping step from TorsoSharp - Added new C# project SysInfoDump
    The new SysInfoDump.exe will be used as a helper exe by muFAT nodes to periodically collect system information and save to a text file.

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="muvee Technologies Pte Ltd">
+//   Copyright (c) muvee Technologies Pte Ltd. All rights reserved.
+// </copyright>
+// <author>Jerry Chong</author>
+//-----------------------------------------------------------------------
+
+namespace SysInfoDump
+{
+    using System.Diagnostics;
+    using System.IO;
+    using SysInfoSharp;
+
+    /// <summary>
+    /// Main program
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Program entry function
+        /// </summary>
+        /// <param name="args">Command line arguments</param>
+        public static void Main(string[] args)
+        {
+            // specify writing destination
+            string outputFile = Path.Combine(@"C:\muveeDebug", "sysinfoout.txt");
+            if (args.Length > 0)
+            {
+                outputFile = args[0];
+            }
+
+            using (var strm = File.Open(outputFile, FileMode.Create))
+            {
+                // initialize loggers - any output written to the file will also be printed
+                Trace.Listeners.Clear();
+                Trace.Listeners.Add(new ConsoleTraceListener());
+                Trace.Listeners.Add(new TextWriterTraceListener(strm));
+                Trace.AutoFlush = true;
+
+                // dumping system info
+                var sysInfo = new SysInfoLib();
+                sysInfo.Init();
+                foreach (var category in sysInfo.GetCategories())
+                {
+                    Trace.WriteLine("--------------------------------------------");
+                    Trace.WriteLine("Category: " + category);
+                    Trace.WriteLine("--------------------------------------------");
+
+                    foreach (var pair in sysInfo[category])
+                    {
+                        Trace.WriteLine(string.Format("\t{0}={1}", pair.Key, pair.Value));
+                    }
+                }
+            }
+        }
+    }
+}
